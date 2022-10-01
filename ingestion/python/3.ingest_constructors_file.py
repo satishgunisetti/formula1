c10a2528491dad617621800dcd9a0e711d713ce7
satishgunisetti <satishgunisetti>
2022-10-01 04:50:09 +0000

    Adding the constructor ingestion scripts

---
+++ ingestion/python/3.ingest_constructors_file.py
@@ -0,0 +1,45 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### Ingesting constructors.csv file from Mounted ADLS container  
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC head /mnt/formula1dlepam/raw/constructors.json
+
+# COMMAND ----------
+
+schema = 'constructorId INT, constructorRef STRING, name STRING, nationality STRING, url STRING'
+
+# COMMAND ----------
+
+cons_df = ( spark.read.format('json')
+               .schema(schema)
+               .load('/mnt/formula1dlepam/raw/constructors.json')
+          )
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Select and derive the required columns from the dataframe
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp
+
+# COMMAND ----------
+
+cons_transformed_df = ( cons_df.withColumnRenamed('constructorId', 'constructor_id')
+                               .withColumnRenamed('constructorRef', 'constructor_ref')
+                               .withColumn('ingestion_date', current_timestamp())
+                               .select('constructor_id', 'constructor_ref', 'name', 'nationality', 'ingestion_date')
+                      )
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Write data to datalake as parquet file
+
+# COMMAND ----------
+
+cons_transformed_df.write.mode('overwrite').parquet('dbfs:/mnt/formula1dlepam/processed/constructors')
---
+++ ingestion/sql/3.ingest_constructors_file.sql
@@ -0,0 +1,50 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC 
+-- MAGIC ### Using SQL we are loading the data from circuits.csv 
+
+-- COMMAND ----------
+
+CREATE OR REPLACE TEMP VIEW constructors_tv AS
+
+SELECT * FROM JSON.`/mnt/formula1dlepam/raw/constructors.json`
+
+-- COMMAND ----------
+
+-- selecting the required columns and defining the ingestion date column
+
+CREATE OR REPLACE TEMP VIEW constructors_transformed AS
+SELECT CAST(constructorId AS INT)  AS constructor_id
+     , constructorRef AS constructor_ref
+     , name
+     , nationality
+     , CURRENT_TIMESTAMP() AS ingestion_date
+FROM constructors_tv
+
+-- COMMAND ----------
+
+-- creating the external table
+USE processed;
+
+CREATE TABLE IF NOT EXISTS constructors
+(constructor_id INT, constructor_ref STRING, name STRING, nationality STRING, ingestion_date TIMESTAMP)
+USING PARQUET
+LOCATION 'dbfs:/mnt/formula1dlepam/processed/processed.db/constructors'
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### Write data to datalake as parquet file
+
+-- COMMAND ----------
+
+INSERT OVERWRITE constructors
+SELECT * FROM constructors_transformed
+
+-- COMMAND ----------
+
+REFRESH TABLE constructors
+
+-- COMMAND ----------
+
+SELECT * FROM constructors;
